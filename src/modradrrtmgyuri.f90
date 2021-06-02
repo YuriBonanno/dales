cbdfa4a18599d94fa5254a436debbad92c87a5a5
YuriBonanno <YuriBonanno@hotmail.com>
2021-06-02 16:48:35 +0200

    Added some prints so that diagnostics occur

--- src/modradrrtmgyuri.f90
+++ src/modradrrtmgyuri.f90
@@ -642,12 +642,14 @@ contains
 
 					cloudtop_GLQ_point = temp_GLQ_point - n_GLQ_clear
 					class_number = cloudtop_GLQ_point/class_size
-					cloudtop_GLQ_point = MODULO(cloudtop_GLQ_point, class_size)
 					if (MODULO(cloudtop_GLQ_point, class_size) > 0) then
 						class_number = class_number + 1
 					end if
-					cloudtop_GLQ_point = cloudtop_GLQ_point + 1
+					cloudtop_GLQ_point = cloudtop_GLQ_point - class_number*class_size
+					!cloudtop_GLQ_point = cloudtop_GLQ_point + 1
 					
+					print *, "class_size"
+					print *, class_size
 					print *, "class_number"
 					print *, class_number
 					print *, "cloudtop_GLQ_point"
