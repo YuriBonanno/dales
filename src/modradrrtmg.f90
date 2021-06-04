17950dbbe7f3bfa0f4eaf1cad8e985b71719eea8
YuriBonanno <YuriBonanno@hotmail.com>
2021-06-04 15:29:49 +0200

    defined the arrays in setupBarkerSlices

--- src/modradrrtmg.f90
+++ src/modradrrtmg.f90
@@ -966,9 +966,14 @@ contains
 	sstxy(:,:) = 0.
 	tabs_slice(:,:) = 0.; qv_slice(:,:) = 0.; qcl_slice(:,:) = 0.; qci_slice(:,:) = 0.;
 	rho_slice(:,:) = 0.
+	!!Added these to define the whole array without it being read to be some unknown value and breaking everything
+	o3_slice(:,:) = 0. ; o3vmr(:,:) = 0. ; 
+	co2vmr(:,:) = 0. ; ch4vmr(:,:) = 0. ; n2ovmr(:,:) = 0. ; h2ovmr(:,:) = 0. ; o2vmr(:,:) = 0. ; 
+	cfc11vmr(:,:) = 0. ; cfc12vmr(:,:) = 0. ; cfc22vmr(:,:) = 0. ; ccl4vmr(:,:) = 0. ; 
+	layerP(:,:) = 0. ; interfaceP(:,:) = 0. ; layerT(:,:) = 0. ; interfaceT(:,:) = 0. ;
+	layerMass(:,:) = 0. ; LWP_slice(:,:) = 0. ; IWP_slice(:,:) = 0. ; o3_slice(:,:) = 0. ; 
 
 	exners = (ps/pref0) ** (rd/cp)
-
 
 	temp_GLQ_point = passed_GLQ_point
 	print *, "temp_GLQ_point, passed_GLQ_point"
