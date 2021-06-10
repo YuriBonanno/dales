1171e8bc15170259aba3b4cf5afd2ce2beff342f
YuriBonanno <YuriBonanno@hotmail.com>
2021-06-10 14:44:54 +0200

    added more prints

--- src/modradrrtmgyuri.f90
+++ src/modradrrtmgyuri.f90
@@ -708,11 +708,13 @@ contains
 					n2 = cloudtop_GLQ_point
 					
 					print *, "start through LWP indexes"
+					print *, "class_number", class_number
 					print *, "n1", n1
 					print *, "n2", n2
 					do n = n1, n2
-						fill_i = original_cloudtop_LWP_indexes(n, 1, class_number)
-						fill_j = original_cloudtop_LWP_indexes(n, 2, class_number)
+						print *, "n", n
+						fill_i = int(original_cloudtop_LWP_indexes(n, 1, class_number))
+						fill_j = int(original_cloudtop_LWP_indexes(n, 2, class_number))
 						
 						print *, "A"
 						print *, fill_i
