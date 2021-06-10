53ecafa74a0b66e60c8ebcfe48de7467e1e41b41
YuriBonanno <YuriBonanno@hotmail.com>
2021-06-10 18:00:02 +0200

    moved to new dir

--- src/modradrrtmgyuriroutines.f90
+++ src/modradrrtmgyuriroutines.f90
@@ -165,7 +165,7 @@ contains
 		makedir = "datadir"
 		!call execute_command_line ('mkdir -p out/' // adjustl(trim( makedir ) ) )
 		call execute_command_line ('mkdir -p ' // makedir)
-		filename = (makedir // '/' // filename)
+		filename = makedir // '/' // filename
 
 		
 
