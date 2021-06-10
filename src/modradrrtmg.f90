23cde45723a32dddd25ec51001533580f47b7ba1
YuriBonanno <YuriBonanno@hotmail.com>
2021-06-10 13:47:57 +0200

    edited now faulty indexes

--- src/modradrrtmg.f90
+++ src/modradrrtmg.f90
@@ -1011,15 +1011,15 @@ contains
 			temp_j = GLQ_index_all(temp_GLQ_point,2)
 		end if
 
-		tg_slice(i) = tskin(temp_i+1,temp_j+1) * exners  ! Note: tskin = thlskin...
+		tg_slice(i) = tskin(temp_i,temp_j) * exners  ! Note: tskin = thlskin...
 		do k=1,kmax
 			!!! +1 in j must be checked with Stephan
-			tabs_slice(i,k) = thl0(temp_i+1,temp_j+1,k) * exnf(k) &
-								+ (rlv / cp) * ql0(temp_i+1,temp_j+1,k)
+			tabs_slice(i,k) = thl0(temp_i,temp_j,k) * exnf(k) &
+								+ (rlv / cp) * ql0(temp_i,temp_j,k)
 								
 			!!! +1 in j must be checked with Stephan
-			qv_slice  (i,k) = max(qt0(temp_i+1,temp_j+1,k) - ql0(temp_i+1,temp_j+1,k),1e-18) !avoid RRTMG reading negative initial values 
-			qcl_slice (i,k) = ql0(temp_i+1,temp_j+1,k)
+			qv_slice  (i,k) = max(qt0(temp_i,temp_j,k) - ql0(temp_i,temp_j,k),1e-18) !avoid RRTMG reading negative initial values 
+			qcl_slice (i,k) = ql0(temp_i,temp_j,k)
 			qci_slice (i,k) = 0.
 			o3_slice  (i,k) = o3snd(npatch_start) ! o3 constant below domain top (if usero3!)
 
