46559d76971fe31c2e4943cdc1de5b1adbe96ce8
YuriBonanno <YuriBonanno@hotmail.com>
2021-06-11 18:36:30 +0200

    testing setting up barkerslices

--- src/modradrrtmg.f90
+++ src/modradrrtmg.f90
@@ -251,11 +251,11 @@ contains
 			end if
 			
 			passed_GLQ_point = current_GLQ_point
-			print *, "Starting  setupBarkerSlicesFromProfiles"
+			!!! print *, "Starting  setupBarkerSlicesFromProfiles"
 			call setupBarkerSlicesFromProfiles(npatch_start, &
 			   LWP_slice, IWP_slice, cloudFrac, liquidRe, iceRe, &
 			   passed_GLQ_point, total_amount_GLQ_points, GLQ_index_all, passed_slice_length)
-			print *, "Finished  setupBarkerSlicesFromProfiles"
+			!!! print *, "Finished  setupBarkerSlicesFromProfiles"
 			
 			! call setupSlicesFromProfiles &
 			   ! ( j+1, npatch_start, &                                           !input
