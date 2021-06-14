831688219e54b6a6e3fe54bfc3b62e7a3c985e7d
YuriBonanno <YuriBonanno@hotmail.com>
2021-06-14 14:08:04 +0200

    potentially fixed bug

--- src/modradrrtmg.f90
+++ src/modradrrtmg.f90
@@ -1064,7 +1064,7 @@ contains
   integer :: total_amount_GLQ_points
   integer :: passed_GLQ_point, temp_GLQ_point
   integer :: slice_length
-  integer,dimension(:,:) :: GLQ_index_all (total_amount_GLQ_points, 2)
+  integer, allocatable ,dimension(:,:) :: GLQ_index_all (total_amount_GLQ_points, 2)
   real (KIND=kind_rb) :: exners
   real(KIND=kind_rb) :: layerMass(imax,krad1)
   !real(KIND=kind_rb),dimension(imax,kmax)     :: tabs         ! Absolute temperature
