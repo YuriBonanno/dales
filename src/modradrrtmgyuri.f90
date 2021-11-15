9969bc612fe616ea0d3a03f627210cb729b5742d
YuriBonanno <YuriBonanno@hotmail.com>
2021-11-15 18:27:08 +0100

    added new tech with the classes and dynamic GLQ points

--- src/modradrrtmgyuri.f90
+++ src/modradrrtmgyuri.f90
@@ -681,12 +681,8 @@ contains
 		print *, "original indexes cloudtop"
 		if (n_GLQ_cloudtop>0) then
 			do i=1,n_classes
-				print *, "i", i
-				do j= 1,n_in_class(n)
+				do j= 1,n_in_class(i)
 					GLQ_counter = GLQ_counter + 1
-					! print *, "GLQ_counter", GLQ_counter
-					
-					print *, "j", j
 					Original_index_all(GLQ_counter, 1) = original_cloudtop_LWP_indexes(j, 1, i)
 					Original_index_all(GLQ_counter, 2) = original_cloudtop_LWP_indexes(j, 2, i)
 				enddo
