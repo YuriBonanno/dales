241540d6c3276cb229a53143fff9f68613026122
YuriBonanno <YuriBonanno@hotmail.com>
2021-12-01 22:44:01 +0100

    Broken Rico

--- src/modradrrtmg.f90
+++ src/modradrrtmg.f90
@@ -1738,7 +1738,7 @@ contains
 	! call cpu_time(startTime)
 	call StephanRad(sunUp)
 	
-	call writetofiledefinedsize("TESTLWU", lwu(x1:x2,y1:y2, LWP_index(2)), 2, 64, 64, 1, .true.)
+	call writetofiledefinedsize("TESTLWU", lwu(4:67,4:67, LWP_index(2)), 2, 64, 64, 1, .true.)
 	call writetofiledefinedsize("TESTCLOUDFRAC", cloudFracModRad, 2, 64, 64, 1, .true.)
 	
 	!PUT ALL THE TEMPRADS FROM STEPHAN INTO FILES
