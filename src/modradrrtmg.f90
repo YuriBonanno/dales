6b3630e196c12287c917beaae2095af5430aacee
YuriBonanno <YuriBonanno@hotmail.com>
2021-12-02 13:16:27 +0100

    broken indexing

--- src/modradrrtmg.f90
+++ src/modradrrtmg.f90
@@ -1738,7 +1738,7 @@ contains
 	! call cpu_time(startTime)
 	call StephanRad(sunUp)
 	
-	call writetofiledefinedsize("TESTLWU", lwu(4:67,4:67, LWP_index(2)), 2, 64, 64, 1, .false.)
+	call writetofiledefinedsize("TESTLWU", lwu(1:70,1:70, LWP_index(2)), 2, 70, 70, 1, .false.)
 	call writetofiledefinedsize("TESTCLOUDFRAC", cloudFracModRad, 2, 64, 64, 1, .false.)
 	
 	!PUT ALL THE TEMPRADS FROM STEPHAN INTO FILES
